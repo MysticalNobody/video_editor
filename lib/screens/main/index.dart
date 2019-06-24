9e303c9c043505b4b5180607dc77de692f387ab5
Kit <kit@itis.team>
2019-06-24 07:36:56 +0300

    remove useless code

--- lib/screens/main/index.dart
+++ lib/screens/main/index.dart
@@ -7,36 +7,13 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  PageController pageController =
-      PageController(initialPage: 0, keepPage: true);
-  int page = 0;
-
-  var backScrollController = ScrollController(initialScrollOffset: 426);
-  Size size;
   @override
   void initState() {
     super.initState();
-    pageController.addListener(() {
-      animate();
-    });
-  }
-
-  animate() {
-    var value = pageController.offset;
-    if (value < 0) value = 0;
-    if (value > size.width * 2) value = size.width * 2;
-    backScrollController.jumpTo(mapValue(
-        value, 0, size.width * 2, 0, (5060 * size.height / 1956) * 1 / 3));
-  }
-
-  double mapValue(
-      double x, double inMin, double inMax, double outMin, double outMax) {
-    return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
   }
 
   @override
   Widget build(BuildContext context) {
-    size = MediaQuery.of(context).size;
     return WillPopScope(
         onWillPop: () async {
           return true;
